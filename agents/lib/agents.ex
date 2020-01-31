27cb4017070456994d57cab53e850ba5aef76dd6
Kristian Flikka <kristian.flikka@gmail.com>
2020-01-31 16:00:07 +0100

    Get newest png, and start poking a bit

--- agents/lib/agents.ex
+++ agents/lib/agents.ex
@@ -17,11 +17,19 @@ defmodule Agents do
     {:ok, folder_content} = File.ls(folder)
     pngs = Enum.filter(folder_content, fn x -> Path.extname(x) == ".png" end)
     sorted_pngs = Enum.sort(pngs)
-    IO.puts "The sorted PNGs are:"
-    IO.puts Enum.join(sorted_pngs,"\n")
+    IO.puts "The newest png is #{List.last(sorted_pngs)}"
+    IO.puts "The oldest png is #{List.first(sorted_pngs)}"
+
     imgs = Enum.map(sorted_pngs, fn x -> Imagineer.load(Path.join([folder, x])) end)
 
+    png_dict = Enum.into(Enum.zip(sorted_pngs, imgs), %{})
 
+    {:ok, newest_img} = png_dict[List.first(sorted_pngs)]
+
+    IO.puts "Length first dim #{length(newest_img.pixels)}"
+    IO.puts "Length second dim #{length(Enum.at(newest_img.pixels, 0))}"
+
+    # Now, send an image out to a process?
     :readok
   end
 end
